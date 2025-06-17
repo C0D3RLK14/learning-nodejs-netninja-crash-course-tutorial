74ec06a417176058f538661cd0966af3f16c4158
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-17 20:56:17 +0530

    Localhost and port number
    Finished the part localhost and port number and finished the tutorial

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = http.createServer((req, res) => {
 }); 
 // NOTE: We don't have store the server in a variable in our case but for some purposes like using web sockets we would have to.
 
+// This make the server functional by making it listen to a port
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000');
 });
