d045bc1209b3ea5d43915684df813896109ff605
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-19 17:57:27 +0530

    Passing data into views
    Passing data into views part finished

--- app.js
+++ app.js
@@ -18,19 +18,29 @@ app.get('/', (req,res) => {
     // // passing and html file
     // res.sendFile('./views/index.html', {root:__dirname}); 
 
-    // serving the index file as a response
-    res.render('index'); // This is called rendering a view
+    // // serving the index file as a response
+    // res.render('index'); // This is called rendering a view
+
+    // passing a list
+    const blogs = [
+        {title : 'Yoshi finds eggs', snippet : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus natus eius ipsa atque ipsum omnis et rerum repudiandae ducimus. Asperiores maxime amet obcaecati earum culpa facere consequuntur ut excepturi reiciendis.'},
+        {title : 'Mario finds eggs', snippet : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus natus eius ipsa atque ipsum omnis et rerum repudiandae ducimus. Asperiores maxime amet obcaecati earum culpa facere consequuntur ut excepturi reiciendis.'},
+        {title : 'How to defeat bowser', snippet : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus natus eius ipsa atque ipsum omnis et rerum repudiandae ducimus. Asperiores maxime amet obcaecati earum culpa facere consequuntur ut excepturi reiciendis.'}
+    ];
+
+    // passing data from app.js to index view
+    res.render('index', {title : 'Home', blogs}); // With the '.render' method we could pass through an object to the view being rendered. In it we could pass the values we need to pass into the view
 });
 
 // Testing app.get again
 app.get('/about', (req,res) => {
     // res.sendFile('./views/about.html', {root:__dirname});
-    res.render('about')
+    res.render('about', {title : 'About'})
 });
 
 // create blog
 app.get('/blogs/create', (req,res) => {
-    res.render('create');
+    res.render('create', {title : 'Create a blog'});
 });
 
 /* Redirects */
@@ -41,7 +51,7 @@ app.get('/about-us', (req,res) => {
 /* 404 page */
 app.use((req,res) => {
     // res.status(404).sendFile('./views/404.html', {root : __dirname});
-    res.status(404).render('404');
+    res.status(404).render('404', {title : '404'});
 });
 
 
