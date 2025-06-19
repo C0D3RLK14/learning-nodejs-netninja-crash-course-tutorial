31225d0e2aa5e1a92572b09f5bc00493dd80ce52
C0D3RLK14 <lk12karunanayaka@gmail.com>
2025-06-19 20:08:24 +0530

    Clean. up

--- app.js
+++ app.js
@@ -5,22 +5,13 @@ const express = require('express');
 const app = express(); 
 
 /* Registering EJS view engine */
-app.set('view engine', 'ejs'); // '.set' is like a method used to set settings in express. Now we changed the view engine to ejs
-
-// NOTE: By default express and ejs look for views (HTML files) in the views directly (which netninja made us create before saying so). We could change it if we want using the following code
-// app.set('views', 'myviews') // 'views' => 'myviews' folder
+app.set('view engine', 'ejs'); 
 
 //listen for requests
 app.listen(3000); 
 
 //listening for get requests' url
 app.get('/', (req,res) => {
-    // // passing and html file
-    // res.sendFile('./views/index.html', {root:__dirname}); 
-
-    // // serving the index file as a response
-    // res.render('index'); // This is called rendering a view
-
     // passing a list
     const blogs = [
         {title : 'Yoshi finds eggs', snippet : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus natus eius ipsa atque ipsum omnis et rerum repudiandae ducimus. Asperiores maxime amet obcaecati earum culpa facere consequuntur ut excepturi reiciendis.'},
@@ -29,12 +20,11 @@ app.get('/', (req,res) => {
     ];
 
     // passing data from app.js to index view
-    res.render('index', {title : 'Home', blogs}); // With the '.render' method we could pass through an object to the view being rendered. In it we could pass the values we need to pass into the view
+    res.render('index', {title : 'Home', blogs}); 
 });
 
 // Testing app.get again
 app.get('/about', (req,res) => {
-    // res.sendFile('./views/about.html', {root:__dirname});
     res.render('about', {title : 'About'})
 });
 
@@ -50,7 +40,6 @@ app.get('/about-us', (req,res) => {
 
 /* 404 page */
 app.use((req,res) => {
-    // res.status(404).sendFile('./views/404.html', {root : __dirname});
     res.status(404).render('404', {title : '404'});
 });
 
